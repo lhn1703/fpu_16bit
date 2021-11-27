35458d1e5b53d0ddd81be0bb7edd479fb272e734
lhn1703 <55109682+lhn1703@users.noreply.github.com>
2021-11-27 11:15:26 -0800

    completed module, not tested

--- addSubCircuit.v
+++ addSubCircuit.v
@@ -1,5 +1,5 @@
 module addSubCircuit (
-    output reg [1:0] OFUF, output reg done, output [15:0] result,
+    output reg [1:0] OFUF, output reg done, output reg [15:0] result,
     input [15:0] X, Y,
     input addSub, reset, clk
     ); //addSub is 0 if add, 1 if subtract
@@ -10,16 +10,18 @@ module addSubCircuit (
     reg [3:0] state;
     reg xSign, ySign, zSign;
     reg [4:0] xExp, yExp, zExp;
-    reg [10:0] xMan, yMan, zMan;
+    reg [10:0] xMan, yMan, zMan; //hidden bit
 
     reg xBigger;
 
-    assign result = {zSign, zExp, zMan};
-
     reg [4:0] tempExp;
-    reg [10:0] tempMan;
-
-    reg [11:0] addSubManTemp; //hidden and overflow bit
+    reg [10:0] tempMan; //state 3 continuous assign
+    reg [11:0] addSubManTemp; //hidden and overflow bit state 5 continouous assign
+    
+    reg [10:0] subNormTemp; //state 9 continouous assign 
+    reg [4:0] tempZExp; //state 9 continuous assign
+    wire normalized; //state 9 boolean check
+    assign normalized = (subNormTemp[10] == 1'b1); //state 9 normalized check
 
     always @ (*) begin //combinational logic
         //state 3 loop check
@@ -37,12 +39,17 @@ module addSubCircuit (
             zSign = xSign;
         end else begin
             if (xBigger) 
-                zMan = xMan - yMan;
+                addSubManTemp = xMan - yMan;
             else 
-                zMan = yMan - xMan;
+                addSubManTemp = yMan - xMan;
             zSign = (xSign == xBigger); //negative larger x means neg exponent
         end
 
+        //state 9 loop check
+        if (addSubManTemp[10] == 1'b0) begin//if the hidden bit is 0
+            subNormTemp = zMan << 1;
+            tempZExp = zExp - 1;
+        end
     end
 
     always @ (posedge clk or posedge reset) begin
@@ -65,18 +72,11 @@ module addSubCircuit (
                     xBigger <= 0;    
             end
             done <= 0;
+            OFUF <= 2'b0;
         end
         else begin
             case (state) 
                 0: begin
-                    // xSign <= X[15];
-                    // ySign <= Y[15];
-                    // xExp <= X[14:10];
-                    // yExp <= Y[14:10];
-                    // xMan <= X[9:0];
-                    // yMan <= Y[9:0];
-                    // done <= 0;
-
                     if (X == 0 || Y == 0)
                         state <= 1;
                     else begin
@@ -143,29 +143,59 @@ module addSubCircuit (
                     done <= 1;
                 end        
                 5: begin
-                    
+                    zExp <= xExp; //doesn't matter which one gets chosen they are equal
+                    zMan <= addSubManTemp[10:0]; //initialize the zMan for state 9
+                    if (addSubManTemp == 0) begin
+                        state <= 6;
+                    end else begin
+                        if (addSubManTemp[11] == 1'b1) //if the mantissa result msb is a 1 (overflow)
+                            state <= 7;
+                        else begin
+                            if (addSubManTemp[10] == 1'b0) //if the hidden bit 0, need to left shift to normalize
+                                state <= 9;
+                            else 
+                                state <= 11; //else if the overflow msb == 0 and hidden bit == 1, then number is normalized
+                        end
+                    end
                 end    
+                6: begin
+                    result <= 16'b0;
+                    done <= 1;
+                end
+                7: begin //addition overflow state
+                    if (zExp == 5'b11110) //mantissa overflow with maximum representable exponent
+                        state <= 8;
+                    else begin //else handles addition overflow (normalize rightwards)
+                        zMan <= addSubManTemp >> 1;
+                        zExp <= zExp + 1;
+                        state <= 11; //will be normalized already after shifting
+                    end
+                end
+                8: begin //exponent overflow
+                    result <= 16'bz;
+                    OFUF <= 2'b10;
+                end
+                9: begin //subtraction underflow state 
+                    if (zExp == 5'b0) //mantissa underflow with minimum representable exponent
+                        state <= 10;
+                    else begin
+                        zMan <= subNormTemp;
+                        zExp <= tempZExp;
+                        if (normalized) //normalized signal will be set asynchronously before zMan grabs the correct result on the posedge
+                            state <= 11;
+                        else //repeat the computations this state
+                            state <= 9;
+                    end
+                end
+                10: begin //exponent underflow
+                    OFUF <= 2'b01;
+                    done <= 1;
+                end
+                11: begin
+                    result <= {zSign, zExp, zMan[9:10]};
+                    done <= 1;
+                end
             endcase
         end
     end
 endmodule
-
-    /*always @ (posedge clk or posedge reset) begin
-        if (reset) begin
-            state <= 0;
-            xSign <= X[15];
-            ySign <= Y[15];
-            xExp <= X[14:10];
-            yExp <= Y[14:10];
-            xMan <= X[9:0];
-            yMan <= Y[9:0];
-        end else begin
-            case(state) 
-                0: begin
-                    
-                end
-
-            endcase
-        end
-
-    end*/
